d8513bb6e6661c7e4a0fa887e0b601f7f5e7d3e8
Robby-Blue <69482496+Robby-Blue@users.noreply.github.com>
2026-08-15 14:26:13 +0200

    add spiegel news

---
+++ scripts/SpiegelBrief/main.py
@@ -0,0 +1,68 @@
+import json
+import requests
+import xml.etree.ElementTree as ET
+
+
+def get_articles():
+    r = requests.get("https://www.spiegel.de/politik/index.rss")
+    
+    print(f"{r.status_code=}")
+    
+    root = ET.fromstring(r.text)
+    channel = root.find("channel")
+    articles_xml = channel.findall("item")
+    
+    print(f"{len(articles_xml)=}")
+    
+    articles = []
+    
+    for article_xml in articles_xml:
+        title = article_xml.find("title").text
+        description = article_xml.find("description").text
+        link = article_xml.find("link").text
+        
+        article = {
+            "title": title,
+            "description": description,
+            "link": link
+        }
+        
+        articles.append(article)
+    
+    return articles
+    
+def discord_webhook_from_articles(articles):
+    
+    return {
+        "title": "SpiegelNews",
+        "embeds": [
+            {
+                "fields": [
+                    field_from_article(article)
+                    for article in articles[:9]
+                ]
+            }
+        ]
+    }
+
+def field_from_article(article):
+    title = article["title"]
+    description = article["description"]
+    link = article["link"]
+    
+    text = f"{description} [[read more]]({link})"
+    
+    return {
+        "name": title,
+        "value": text,
+        "inline": False
+    }
+
+articles = get_articles()
+
+print(discord_webhook_from_articles(articles))
+
+with open("/app/output/output.json", "w") as f:
+    json.dump({
+        "webhook": discord_webhook_from_articles(articles)
+    }, f)
